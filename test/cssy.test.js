484b6c474692992272ae4465f39a0031566099ce
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-09-25 15:37:54 +0200

    feat: Add global pre/post processing

--- test/cssy.test.js
+++ test/cssy.test.js
@@ -2,6 +2,7 @@
 
 var expect           = require('expect.js')
 var join             = require('path').join
+var cssy             = require('../index.js')
 var processor        = require('../lib/processor')
 var transform        = require('../lib/transform')
 var read             = require('fs').readFileSync
@@ -38,46 +39,100 @@ describe('cssy', function(){
       expect(processor(fixp('filter/notme.css'))).to.be(undefined)
     })
 
-    it('should process a css source', function() {
+    it('should process a css source', function(done) {
       var filename = fixp('basic/source.css');
       var source   = read(filename).toString();
       var proc     = processor(filename);
-      var result   = proc(source);
+      proc(source, function(err, result) {
+        if(err) return done(err);
+        expect(result).to.be.a('object')
+        expect(result).to.have.keys(['imports', 'uid', 'toString'])
+        expect(result.toString()).to.eql("body {\n  font-size: 14px;\n}")
+        done()
+      })
 
-      expect(result).to.be.a('object')
-      expect(result).to.have.keys(['imports', 'uid', 'toString'])
 
-      expect(result.toString()).to.eql("body {\n  font-size: 14px;\n}")
     })
 
     describe('with rework plugin', function() {
-      it('should apply rework plugin', function() {
+      it('should apply rework plugin', function(done) {
         var filename = fixp('rework/source.css');
         var source   = read(filename).toString();
         var proc     = processor(filename);
-        var result   = proc(source);
+        proc(source, function(err, result) {
+          if(err) return done(err);
+          expect(result.toString()).to.eql("body {\n  font-size: 14px;\n}")
+          done()
+        })
 
-        expect(result.toString()).to.eql("body {\n  font-size: 14px;\n}")
       })
 
-      it('should apply rework plugin with arguments', function() {
+      it('should apply rework plugin with arguments', function(done) {
         var filename = fixp('rework-withargs/source.css');
         var source   = read(filename).toString();
         var proc     = processor(filename);
-        var result   = proc(source);
+        proc(source, function(err, result) {
+          if(err) return done(err);
+          expect(result.toString()).to.eql("body {\n  font-size: 14px;\n}")
+          done()
+        })
 
-        expect(result.toString()).to.eql("body {\n  font-size: 14px;\n}")
       })
 
-      it('should apply rework plugin with arguments from external module', function() {
+      it('should apply rework plugin with arguments from external module', function(done) {
         var filename = fixp('rework-withargs-external/source.css');
         var source   = read(filename).toString();
         var proc     = processor(filename);
-        var result   = proc(source);
+        proc(source, function(err, result) {
+          if(err) return done(err);
+          expect(result.toString()).to.eql("body {\n  font-size: 14px;\n}")
+          done()
+        })
+      })
+    })
 
-        expect(result.toString()).to.eql("body {\n  font-size: 14px;\n}")
+    describe('with global pre/post processor', function() {
+
+      afterEach(function() {
+        cssy.reset();
       })
 
+      it('should run pre-process as waterfall', function(done) {
+
+        cssy.pre([
+          function(rw, done) {
+            rw._test = [];
+            rw._test.push('pre1')
+            done(null, rw);
+          },
+          function(rw, done) {
+            rw._test.push('pre2')
+            done(null, rw);
+          },
+        ])
+
+        cssy.post([
+          function(rw, done) {
+            rw._test.push('post1')
+            done(null, rw);
+          },
+          function(rw, done) {
+            rw._test.push('post2')
+            done(null, rw);
+          }
+        ])
+
+        var filename = fixp('basic/source.css');
+        var source   = read(filename).toString();
+        var proc     = processor(filename);
+        proc(source, function(err, result) {
+          if(err) return done(err);
+          expect(result._test).to.eql(['pre1','pre2', 'post1', 'post2'])
+          done()
+        })
+
+
+      })
     })
 
   })
@@ -94,6 +149,16 @@ describe('cssy', function(){
       }))
     })
 
+    it('should process a css source', function(done) {
+      var filename = fixp('rework/source.css');
+      createReadStream(filename)
+      .pipe(transform(filename))
+      .pipe(concatStream(function(result) {
+        expect(result.toString())
+          .to.eql("module.exports = (require('cssy/browser'))(\"body{font-size:14px;}\", '986287257254767c753a0e9ca2097afb', [])")
+        done();
+      }))
+    })
 
     it('should process a css source', function(done) {
       var filename = fixp('rework/source.css');
@@ -101,7 +166,7 @@ describe('cssy', function(){
       .pipe(transform(filename))
       .pipe(concatStream(function(result) {
         expect(result.toString())
-          .to.eql("module.exports = (require('cssy/browser'))(\"body {\\n  font-size: 14px;\\n}\", '986287257254767c753a0e9ca2097afb', [])")
+          .to.eql("module.exports = (require('cssy/browser'))(\"body{font-size:14px;}\", '986287257254767c753a0e9ca2097afb', [])")
         done();
       }))
     })
