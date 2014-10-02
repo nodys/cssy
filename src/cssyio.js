cf5fe062ed6b4917b47466b9c14c7f109b4e2fb9
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-10-02 12:22:44 +0200

    fix: Use lrio.emit() in place of trigger()

--- src/cssyio.js
+++ src/cssyio.js
@@ -1,5 +1,5 @@
 // Export a basic live-reload (lrio) client instance for cssy
 var client = module.exports = require('lrio')('cssy')
 client.on('message', function(data) {
-  client.trigger(data.type + ':' + data.uid, data.src);
+  client.emit(data.type + ':' + data.uid, data.src);
 })
