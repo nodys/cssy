e95ba392fe9e53823bfccb843419fa97c3c1a419
Jean Ponchon <jean.ponchon@novadiscovery.com>
2014-10-02 15:23:17 +0200

    refactor: Cleanup and document cssy-browser api and add CssyBrowser.getImports()

--- src/cssy-browser.js
+++ src/cssy-browser.js
@@ -25,21 +25,55 @@
  *    ctrlSrc.remove(); // Remove source
  *    ctrlSrc.element;  // Inserted `style` element
  *
- * @param  {String} src     Css source
- * @param  {Array} imports  List of imported cssy browser instances
- * @return {Object}         [description]
+ * @param  {String} src
+ *         Css source
+ *
+ * @param  {Array} imports
+ *         List of imported cssy browser instances
+ *
+ * @return {Object}
+ *         Cssy browser instance
  */
 module.exports = function(src, imports) {
 
   var changeListeners = [];
 
-  function cssy() {
-    return cssy.insert.apply(null, arguments)
+  /**
+   * Cssy browser instance is function object
+   *
+   * Can be used as a shortcut to CssyBrowser.insert([to, [media]])
+   *
+   * @return {Object} See CssyBrowser.insert()
+   */
+  function CssyBrowser() {
+    return CssyBrowser.insert.apply(null, arguments)
   }
 
-  cssy.insert = function(to, media) {
+  /**
+   * Insert a `<style>` element in the DOM with current css source
+   *
+   * The content of all the injected style is binded css source changes
+   * (see `CssyBrowser.update()` and `CssyBrowser.onChange()`)
+   *
+   * @param  {[HTMLElement|ShadowRoot]} to
+   *         Where to inject the style. Default to document's head.
+   *
+   * @param  {[String]} media
+   *         Set the media attribute of the injected style tag
+   *
+   * @return {Object}
+   *         An object with:
+   *         - `element` **{HTMLElement}**: The `style` element inserted
+   *         - `imported` **{Array}**: The other CssyBrowser instances imported
+   *           and injected by this instance
+   *         - `remove` **{Function}**: Remove injected `style` element and all
+   *           other CssyBrowser instances imported
+   */
+  CssyBrowser.insert = function(to, media) {
 
     var imported = imports.map(function(imp) {
+      // TODO: What if imp.cssy is not a cssy browser instance
+      // TODO: 'media and ('+imp.media+')' ?
       var sub = imp.cssy(to, imp.media);
       return sub;
     })
@@ -61,19 +95,19 @@ module.exports = function(src, imports) {
     }
 
     function remove() {
-      cssy.offChange(update);
+      CssyBrowser.offChange(update);
       try {
         element.parentNode.removeChild(element);
       } catch(e) {}
       imported.forEach(function(sub) {
         sub.remove();
       })
-      return cssy;
+      return CssyBrowser;
     }
 
     // Initialize:
-    update(cssy.toString())
-    cssy.onChange(update)
+    update(CssyBrowser.toString())
+    CssyBrowser.onChange(update)
 
     return {
       element:  element,
@@ -82,29 +116,62 @@ module.exports = function(src, imports) {
     }
   }
 
-  cssy.toString = function() {
-    return cssy.src;
+  /**
+   * Override default toString()
+   *
+   * @return {String} Current css source
+   */
+  CssyBrowser.toString = function() {
+    return CssyBrowser.src;
   }
 
-  cssy.update = function(src) {
-    cssy.src = src;
+  /**
+   * Update current css source
+   *
+   * Each inject style element are updated too
+   *
+   * @param  {String} src
+   */
+  CssyBrowser.update = function(src) {
+    CssyBrowser.src = src;
     changeListeners.forEach(function(listener) {
       listener(src);
     })
-    return cssy;
+    return CssyBrowser;
   }
 
-  cssy.onChange = function(listener) {
+  /**
+   * Listen for css source changes
+   *
+   * @param {Function} listener
+   *        Change listener. Receive new css source
+   */
+  CssyBrowser.onChange = function(listener) {
     changeListeners.push(listener);
-    return cssy;
+    return CssyBrowser;
   }
 
-  cssy.offChange = function(listener) {
+  /**
+   * Detach change listener
+   *
+   * @param {Function} listener
+   */
+  CssyBrowser.offChange = function(listener) {
     changeListeners = changeListeners.filter(function(l) { return l !== listener })
-    return cssy;
+    return CssyBrowser;
   }
 
-  cssy.src = src;
+  /**
+   * Get the imported CssyBrowser instance (based on `@import` at-rules)
+   *
+   * @return {Array}
+   *         Array of CssyBrowser instance
+   */
+  CssyBrowser.getImports = function() {
+    return imports;
+  }
 
-  return cssy;
+  CssyBrowser.src = src;
+
+  return CssyBrowser;
 }
